42154d64b144910a435d67d59d78670a46fc274d
Alexandr S <alexandr@raisesoftware.com>
2020-06-23 12:11:16 +0300

    Add 'KerberosTicket' class to auth.py.

--- aiohttp_kerberos/auth.py
+++ aiohttp_kerberos/auth.py
@@ -105,3 +105,38 @@ def login_required(function):
                 return web.HTTPForbidden(reason='Invalid authorization header')
         return web.HTTPUnauthorized(reason='Missing authorization token', headers={'WWW-Authenticate': 'Negotiate'})
     return wrapped
+
+
+class KerberosTicket:
+    """
+    Usage:
+        >>> krb = KerberosTicket("HTTP@krbhost.example.com")
+        >>> headers = {"Authorization": krb.auth_header}
+        >>> r = requests.get("https://krbhost.example.com/krb/", headers=headers)
+        >>> r.status_code
+        200
+        >>> krb.verify_response(r.headers["www-authenticate"])
+        >>>
+    """
+    def __init__(self, service):
+        __, krb_context = kerberos.authGSSClientInit(service)
+        kerberos.authGSSClientStep(krb_context, '')
+        self._krb_context = krb_context
+        self.auth_header = ('Negotiate ' + kerberos.authGSSClientResponse(krb_context))
+
+    def verify_response(self, auth_header):
+        # Handle comma-separated lists of authentication fields
+        for field in auth_header.split(','):
+            kind, __, details = field.strip().partition(' ')
+            if kind.lower() == 'negotiate':
+                auth_details = details.strip()
+                break
+        else:
+            raise ValueError('Negotiate not found in %s' % auth_header)
+        # Finish the Kerberos handshake
+        krb_context = self._krb_context
+        if krb_context is None:
+            raise RuntimeError('Ticket already used for verification')
+        self._krb_context = None
+        kerberos.authGSSClientStep(krb_context, auth_details)
+        kerberos.authGSSClientClean(krb_context)
